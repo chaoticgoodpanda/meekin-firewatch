ab34bbfa3823782bb05265bb81968bc1a9bece7c
chaoticgoodpanda <michael.lwin@koekoetech.com>
2021-10-21 18:15:26 +0200

    Added Crowdtangle API keys

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -1,20 +1,29 @@
 using API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace API.Controllers
 {
+
     [ApiController]
     [Route("api/[controller]")]
     public class PostsController : ControllerBase
     {
         private readonly MeekinFirewatchContext _context;
+        private readonly string _fbApiKey;
+        private readonly string _instaApiKey;
+        private readonly string _redditApiKey;
 
-        public PostsController(MeekinFirewatchContext context)
+        public PostsController(MeekinFirewatchContext context, IConfiguration config)
         {
             _context = context;
+            _fbApiKey = config.GetSection("CrowdtangleSettings:FacebookApiKey").Value;
+            _instaApiKey = config.GetSection("CrowdtangleSettings:InstagramApiKey").Value;
+            _redditApiKey = config.GetSection("CrowdtangleSettings:RedditApiKey").Value;
+
         }
         
         [HttpGet]
-        
+        public async 
     }
 }
