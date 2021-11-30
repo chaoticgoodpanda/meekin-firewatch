5f187d353f346d429b69312f13e30cdaa532ca55
chaoticgoodpanda <michael.lwin@koekoetech.com>
2021-11-30 22:46:04 +0100

    Beginning use of Formik

--- client/src/features/threats/threatForm/ThreatForm.tsx
+++ client/src/features/threats/threatForm/ThreatForm.tsx
@@ -16,6 +16,8 @@ import {useStore} from "../../../app/stores/store";
 import {observer} from "mobx-react-lite";
 import {Link, useHistory, useParams} from "react-router-dom";
 import {v4 as uuid} from 'uuid';
+import {Formik} from 'formik';
+import {values} from "mobx";
 
 interface Props {
     post: Post;
@@ -209,76 +211,89 @@ export default observer (function ThreatForm({post}: Props) {
                 noValidate
                 autoComplete="on"
             >
-            <FormControl onSubmit={handleSubmit}>
-                <TextField id="outlined-basic" value={report.summaryAnalysis} name='summaryAnalysis' label="Report title" variant="outlined" onChange={handleInputChange} />
-                <Typography id="input-slider" gutterBottom>
-                    Use the slider to express how dangerous the content is. <br/>
-                    -100 = not dangerous at all. 100 = the most dangerous content.
-                </Typography>
-                <Slider
-                    aria-label="Danger Score"
-                    defaultValue={0}
-                    getAriaValueText={valuetext}
-                    valueLabelDisplay="auto"
-                    step={10}
-                    marks
-                    min={-100}
-                    max={100}
-                    color='secondary'
-                />
-                <FormControl component="fieldset">
-                    <FormLabel component="legend">Who is the speaker?</FormLabel>
-                        <RadioGroup row aria-label="rabat-speaker?" value={radioValue} name="row-buttons-radio-group" onChange={handleRadioChange}>
-                            <FormControlLabel value="Politician" control={<Radio />} label="Politician" color="secondary" />
-                            <FormControlLabel value="Public Figure" control={<Radio />} label="Public Figure" color="secondary" />
-                            <FormControlLabel value="Private Person" control={<Radio />} label="Private Person" color="secondary" />
-                        </RadioGroup>
-                </FormControl>
-                <FormControl component="fieldset">
-                    <FormLabel component="legend">Does the content target a human or group of humans?</FormLabel>
-                    <RadioGroup row aria-label="rabat-speaker?" value={humanRadio} name="row-buttons-radio-group" onChange={handleHumanRadioChange}>
-                        <FormControlLabel value="true" control={<Radio />} label="Yes" color="secondary" />
-                        <FormControlLabel value="false" control={<Radio />} label="No" color="secondary" />
-                    </RadioGroup>
-                </FormControl>
-                <FormControl component="fieldset">
-                    <InputLabel id="demo-multiple-chip-label">Violations in content</InputLabel>
-                    <Select
-                        labelId="demo-multiple-chip-label"
-                        id="demo-multiple-chip"
-                        multiple
-                        value={justification}
-                        onChange={handleChangeJustifications}
-                        input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-                        renderValue={(selected) => (
-                            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-                                {selected.map((value) => (
-                                    <Chip key={value} label={value} />
-                                ))}
-                            </Box>
-                        )}
-                        MenuProps={MenuProps}
-                    >
-                        {rabatJustifications.map((rJustifications) => (
-                            <MenuItem
-                                key={rJustifications}
-                                value={rJustifications}
-                                style={getStyles(rJustifications, justification, theme)}
-                            >
-                                {rJustifications}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
-                <TextField id="filled-multiline-flexible" name='analysisReport' value={report.analysisReport} label="Analysis" variant="filled"
-                           multiline rows={5} onChange={handleInputChange}
-                />
-            </FormControl>
+                <Formik initialValues={report} onSubmit={values => console.log(values)}>
+                    {({values, handleChange, handleSubmit}) => (
+                        <><FormControl onSubmit={handleSubmit}>
+                            <TextField id="outlined-basic" value={report.summaryAnalysis} name='summaryAnalysis'
+                                       label="Report title" variant="outlined" onChange={handleInputChange}/>
+                            <Typography id="input-slider" gutterBottom>
+                                Use the slider to express how dangerous the content is.<br/>
+                                -100 = not dangerous at all. 100 = the most dangerous content.
+                            </Typography>
+                            <Slider
+                                aria-label="Danger Score"
+                                defaultValue={0}
+                                getAriaValueText={valuetext}
+                                valueLabelDisplay="auto"
+                                step={10}
+                                marks
+                                min={-100}
+                                max={100}
+                                color='secondary'/>
+                            <FormControl component="fieldset">
+                                <FormLabel component="legend">Who is the speaker?</FormLabel>
+                                <RadioGroup row aria-label="rabat-speaker?" value={radioValue}
+                                            name="row-buttons-radio-group" onChange={handleRadioChange}>
+                                    <FormControlLabel value="Politician" control={<Radio/>} label="Politician"
+                                                      color="secondary"/>
+                                    <FormControlLabel value="Public Figure" control={<Radio/>} label="Public Figure"
+                                                      color="secondary"/>
+                                    <FormControlLabel value="Private Person" control={<Radio/>} label="Private Person"
+                                                      color="secondary"/>
+                                </RadioGroup>
+                            </FormControl>
+                            <FormControl component="fieldset">
+                                <FormLabel component="legend">Does the content target a human or group of
+                                    humans?</FormLabel>
+                                <RadioGroup row aria-label="rabat-speaker?" value={humanRadio}
+                                            name="row-buttons-radio-group" onChange={handleHumanRadioChange}>
+                                    <FormControlLabel value="true" control={<Radio/>} label="Yes" color="secondary"/>
+                                    <FormControlLabel value="false" control={<Radio/>} label="No" color="secondary"/>
+                                </RadioGroup>
+                            </FormControl>
+                            <FormControl component="fieldset">
+                                <InputLabel id="demo-multiple-chip-label">Violations in content</InputLabel>
+                                <Select
+                                    labelId="demo-multiple-chip-label"
+                                    id="demo-multiple-chip"
+                                    multiple
+                                    value={justification}
+                                    onChange={handleChangeJustifications}
+                                    input={<OutlinedInput id="select-multiple-chip" label="Chip"/>}
+                                    renderValue={(selected) => (
+                                        <Box sx={{display: 'flex', flexWrap: 'wrap', gap: 0.5}}>
+                                            {selected.map((value) => (
+                                                <Chip key={value} label={value}/>
+                                            ))}
+                                        </Box>
+                                    )}
+                                    MenuProps={MenuProps}
+                                >
+                                    {rabatJustifications.map((rJustifications) => (
+                                        <MenuItem
+                                            key={rJustifications}
+                                            value={rJustifications}
+                                            style={getStyles(rJustifications, justification, theme)}
+                                        >
+                                            {rJustifications}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                            <TextField id="filled-multiline-flexible" name='analysisReport'
+                                       value={report.analysisReport} label="Analysis" variant="filled"
+                                       multiline rows={5} onChange={handleInputChange}/>
+                        </FormControl><LoadingButton sx={{ml: 1, mt: 1}} type='submit' onClick={submitRefreshPage}
+                                                     color="success"
+                                                     variant="outlined" loading={loading}>Submit
+                            Report</LoadingButton><Button component={Link} to={'/threats'} sx={{ml: 1, mt: 1}}
+                                                          type='submit' color="error"
+                                                          variant="outlined">Cancel</Button></>
+                    )}
+                </Formik>
+            
             </Box>
-            <LoadingButton sx={{ml: 1, mt: 1}} type='submit' onClick={submitRefreshPage} color="success"
-                    variant="outlined" loading={loading}>Submit Report</LoadingButton>
-            <Button component={Link} to={'/threats'} sx={{ml: 1, mt: 1}} type='submit' color="error"
-                    variant="outlined">Cancel</Button>
+
         </>
           
     )
